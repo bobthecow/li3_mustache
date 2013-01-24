f0c8998d74fe6a1feafcb301f98c9268a9aae0b0
Justin Hileman <justin@justinhileman.info>
2013-01-24 00:42:49 -0800

    Update Mustache adapter to use Mustache_Engine
    Add some additional default configs for Mustache.php v2.x

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -7,10 +7,20 @@ Media::type('mustache', 'text/x-mustache', array(
 	'loader' => 'Mustache',
 	'renderer' => 'Mustache',
 	'paths' => array(
-		'template' => '{:library}/views/{:controller}/{:template}.{:type}',
-		'layout'   => '{:library}/views/layouts/{:layout}.{:type}',
-		'element'  => '{:library}/views/elements/{:template}.{:type}'
-	)
+		'template' => array(
+			LITHIUM_APP_PATH . '/views/{:controller}/{:template}.{:type}.mustache',
+			'{:library}/views/{:controller}/{:template}.{:type}.mustache',
+		),
+		'layout'   => array(
+			LITHIUM_APP_PATH . '/views/layouts/{:layout}.{:type}.mustache',
+			'{:library}/views/layouts/{:layout}.{:type}.mustache',
+		),
+		'element'  => array(
+			LITHIUM_APP_PATH . '/views/elements/{:template}.{:type}.mustache',
+			'{:library}/views/elements/{:template}.{:type}.mustache',
+		)
+	),
+	'cache' => LITHIUM_APP_PATH . '/resources/tmp/cache/mustache',
 ));
 
 ?>
